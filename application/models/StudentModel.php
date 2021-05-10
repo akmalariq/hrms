9c16ac9d5c4bcf4ede8663abe964c708b740bd68
akmalariq <santoso54321@gmail.com>
2021-05-10 08:45:30 +0700

    add Signup

---
+++ application/models/StudentModel.php
@@ -0,0 +1,58 @@
+<?php
+
+class StudentModel extends CI_model {
+    public function getAllMahasiswa()
+    {
+        // $query = $this->db->get('mahasiswa');
+        // return $query->result_array();
+
+        // Chaining
+        return $this->db->get('mahasiswa')->result_array();
+    }
+
+    public function addSignup()
+    {
+        $data = [
+            "username" => $this->input->post('username', true),
+            "email" => $this->input->post('email', true),
+            "password" => $this->input->post('password', true)
+        ];
+        echo var_dump($data);
+
+        $this->db->insert('student', $data);
+    }
+
+    public function hapusDataMahasiswa($id)
+    {
+        // $this->db->where('id', $id);
+        $this->db->delete('mahasiswa', ['id' => $id]);
+    }
+
+    public function getMahasiswaById($id)
+    {
+        return $this->db->get_where('mahasiswa', ['id' => $id])->row_array();
+    }
+
+    public function ubahDataMahasiswa()
+    {
+        $data = [
+            "nama" => $this->input->post('nama', true),
+            "nrp" => $this->input->post('nrp', true),
+            "email" => $this->input->post('email', true),
+            "jurusan" => $this->input->post('jurusan', true)
+        ];
+
+        $this->db->where('id', $this->input->post('id'));
+        $this->db->update('mahasiswa', $data);
+    }
+
+    public function cariDataMahasiswa()
+    {
+        $keyword = $this->input->post('keyword', true);
+        $this->db->like('nama', $keyword);
+        $this->db->or_like('jurusan', $keyword);
+        $this->db->or_like('nrp', $keyword);
+        $this->db->or_like('email', $keyword);
+        return $this->db->get('mahasiswa')->result_array();
+    }
+}
